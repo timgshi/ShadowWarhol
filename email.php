dec29bf07a7b1d3fa8c589e0596ba6d1cc96c6c2
Tim Shi <timgshi@gmail.com>
2013-01-19 15:52:04 -0800

    email sending with attachment works

--- email.php
+++ email.php
@@ -3,10 +3,14 @@
 	require("postmark.php");
 	
 	$postmark = new Postmark("8e1b879a-1100-45aa-9fd6-36463856455f","timshi@stanford.edu","shadowwarhol@gmail.com");
-	
-	$result = $postmark->to("timgshi@gmail.com")
+	$message = "<html><p>";
+	$message .= $_POST['message'];
+	$message .= "</p>";
+	$message .= "</html>";
+	$result = $postmark->to($_POST['toEmail'])
 		->subject("Test!")
-		->html_message("<b>Test</b>")
+		->html_message($message)
+		->attachment('warhol.jpg', substr($_POST['image-data'], 23), 'application/jpeg')
 		->send();
 	
 	if($result === true)
