b44953f95d0ea05522b6780d8423ad31c2ff6fbd
Shilpa Apte <sapte1@stanford.edu>
2013-01-20 17:33:51 -0800

    timer added

--- js/shadowboxing.js
+++ js/shadowboxing.js
@@ -374,3 +374,48 @@ function updateBackground(i, rCurrent, gCurrent, bCurrent, rBackground, gBackgro
 function pixelDistance(r1, g1, b1, r2, g2, b2) {
     return Math.abs((r1+g1+b1)/3 - (r2+g2+b2)/3);
 }
+
+var Timer;
+var TotalSeconds;
+
+function CreateTimer(TimerID, Time) {
+        Timer = document.getElementById(TimerID);
+        TotalSeconds = Time;
+        
+        UpdateTimer()
+        window.setTimeout("Tick()", 1000);
+}
+
+function Tick() {
+        if (TotalSeconds <= 0) {
+                alert("Time's up!")
+                return;
+        }
+
+        TotalSeconds -= 1;
+        UpdateTimer()
+        window.setTimeout("Tick()", 1000);
+}
+
+function UpdateTimer() {
+        var Seconds = TotalSeconds;
+        
+        var Days = Math.floor(Seconds / 86400);
+        Seconds -= Days * 86400;
+
+        var Hours = Math.floor(Seconds / 3600);
+        Seconds -= Hours * (3600);
+
+        var Minutes = Math.floor(Seconds / 60);
+        Seconds -= Minutes * (60);
+
+
+        var TimeStr = ((Days > 0) ? Days + " days " : "") + LeadingZero(Hours) + ":" + LeadingZero(Minutes) + ":" + LeadingZero(Seconds)
+
+        Timer.innerHTML = TotalSeconds;
+}
+function LeadingZero(Time) {
+
+        return (Time < 10) ? "0" + Time : + Time;
+
+}
